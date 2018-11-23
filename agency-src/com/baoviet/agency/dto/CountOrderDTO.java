adf158ca4ec44ac178ce2d28ae60a01b58e315ec
leminhduc_itsol <leminhduc_itsol@ITSD1-DUCLM.bvgroup.baoviet.vn>
2018-11-23 15:22:19 +0700

    update

---
+++ agency-src/com/baoviet/agency/dto/CountOrderDTO.java
@@ -0,0 +1,34 @@
+package com.baoviet.agency.dto;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * The persistent class for the DepartmentDTO
+ * 
+ */
+@Getter
+@Setter
+@JsonInclude(Include.NON_EMPTY)
+public class CountOrderDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long countCart;
+
+	private Long countBvWaiting;
+	
+	private Long countAgencyWaiting;
+	
+	private Long countOrderMe;
+	
+	private Long countOrderDebit;
+	
+	private Long countOrderExpire;
+	
+	private Long countOrderOther;
+}
